4357e01d441f01156e70dabca96560829be81e27
Your Name <you@example.com>
2023-04-20 11:16:57 +0300

    add header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import '../../style/Header.scss'
+import {BsSearch} from "react-icons/bs";
+import {RiShoppingBagLine} from "react-icons/ri";
+import {NavLink} from "react-router-dom";
+
+
 const Header = () => {
     return (
-        <div id='header'>
+        <div id="header">
             <div className="container">
                 <div className="header">
+                    <div className="header--nav">
+                        <NavLink to={"/book"}>
+                            Bookshop
+                          </NavLink>
+                        <NavLink to={"/genres"}>
+                            Categories
+                        </NavLink>
+                        <NavLink to={"/newBooks"}>
+                            Recent
+                        </NavLink>
+                        <NavLink to={"/Books"}>
+                            Books
+                        </NavLink>
+                        <NavLink to={"/aboutUs"}>
+                            About Us
+                        </NavLink>
 
+                    </div>
+                    <div className="header--input">
+
+                        <BsSearch className="header--input__search"/>
+                        <RiShoppingBagLine className="header--input__bags"/>
+                    </div>
                 </div>
             </div>
-            
         </div>
     );
 };
