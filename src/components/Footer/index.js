aa072434bdba8bd395697cd7383d8c3f3b7923cb
Your Name <you@example.com>
2023-04-22 10:27:02 +0300

    add Footerss

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import '../../style/Footer.scss'
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div id="footer">
+            <div className="container">
+                <div className="footer">
+                    <div className="footer--tittle">
+                        <div className="footer--tittle__email">
+                            <h1>Sign up for our newsletter</h1>
+                            <p>Be the first to know about our special offers, news, and updates.</p>
+                            <input type="text" placeholder="Email Address"/>
+                            <button>Sign Up</button>
+                        </div>
+                        <div className="footer--tittle__text">
+                            <div className="footer--tittle__text--little">
+                                <h1>Lorem Ipsum</h1>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                            </div>
+                            <div className="footer--tittle__text--little">
+                                <h1>Lorem Ipsum</h1>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                            </div>
+                            <div className="footer--tittle__text--little">
+                                <h1>Lorem Ipsum</h1>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                            </div>
+                            <div className="footer--tittle__text--little">
+                                <h1>Lorem Ipsum</h1>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                                <a href="#">lorem</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
