8f1a41c789c9928514b509408977a6a359393b27
Your Name <you@example.com>
2023-04-26 13:13:15 +0300

    last one

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -9,7 +9,7 @@ const AboutUs = () => {
                     <h1 className="AboutUs--us">About Us</h1>
                     <div className="AboutUs--lorem">
                         <img src={Star} alt=""/>
-                        <p className="AboutUs--lorem__page">We believe that books have the power to change lives, and <br/>  we're dedicated to helping our customers find the perfect <br/>  book for them. Whether you're looking for an escape from <br/> reality, an educational read, or a book to inspire you, we've <br/>  got you covered.<br/>
+                        <p className="AboutUs--lorem__page">We believe that books have the power to change lives and books, and <br/>  we're dedicated to helping our customers find the perfect <br/>  book for them. Whether you're looking for an escape from <br/> reality, an educational read, or a book to inspire you, we've <br/>  got you covered.<br/>
                             Thank you so much for choosing our service to shop with us. We look forward to <br/>helping you discover your next favorite book ok!</p>
                     </div>
                 </div>
