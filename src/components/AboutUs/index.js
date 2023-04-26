deadf330f69f490679e9072d460111c8a3383a7c
Your Name <you@example.com>
2023-04-26 13:19:56 +0300

    all fix

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -10,7 +10,7 @@ const AboutUs = () => {
                     <div className="AboutUs--lorem">
                         <img src={Star} alt=""/>
                         <p className="AboutUs--lorem__page">We believe that books have the power to change lives and books, and <br/>  we're dedicated to helping our customers find the perfect <br/>  book for them. Whether you're looking for an escape from <br/> reality, an educational read, or a book to inspire you, we've <br/>  got you covered.<br/>
-                            Thank you so much for choosing our service to shop with us. We look forward to <br/>helping you discover your next favorite book ok!</p>
+                            Thank you so much for choosing our service to shop with us. We look  forward to the <br/>helping you discover your next favorite book ok!</p>
                     </div>
                 </div>
             </div>
